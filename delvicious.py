cc282d0349a8c0b4c39a3e2d2949896a84f1b55e
Steven <lehrburger@gmail.com>
2009-03-10 03:28:34 -0400

    serving xml files of bookmarks from memento85 Delicious account suitable for use as CSE feed

--- delvicious.py
+++ delvicious.py
@@ -1,18 +1,12 @@
-import cgi
-import base64
+import cgi, os, base64
 from xml.dom.minidom import parse, parseString
 from google.appengine.api import urlfetch
 
 from google.appengine.api import users
 from google.appengine.ext import webapp
+from google.appengine.ext.webapp import template
 from google.appengine.ext.webapp.util import run_wsgi_app
 from google.appengine.ext import db
-
-import os
-
-from google.appengine.ext import webapp
-from google.appengine.ext.webapp import template
-
 
 class DelviciousUser(db.Model):
   gaccount = db.UserProperty()
@@ -44,9 +38,6 @@ class Register(webapp.RequestHandler):
     delviciousUser.dpassword = self.request.get('dpassword')
     delviciousUser.put()
     
-    #updateObj = Updater()
-    #updateObj.updateAnnotations(self.request.get('daccount'), self.request.get('dpassword'))
-    
     self.redirect('/search')
 
 class Search(webapp.RequestHandler):
@@ -56,29 +47,27 @@ class Search(webapp.RequestHandler):
 
 class RSSFeedHandler(webapp.RequestHandler):
     def searchHTTP (self, username, password):
-        #url = 'https://' + username + ':' + password + '@api.del.icio.us/v1/posts/all'
-        #url = 'http://lehrblogger.com/nyu/classes/spring09/a2z/midterm/testing.xml'
-        url = 'https://memento85:ont9oth1ag6foc@api.del.icio.us/v1/posts/all'
-    
-        #unpw = base64.b64encode("memento85:ont9oth1ag6foc")'
         res = urlfetch.fetch('https://api.del.icio.us/v1/posts/all', 
-                         headers={'Authorization': 'Basic ' + base64.b64encode("memento85:ont9oth1ag6foc")},
+                         headers={'Authorization': 'Basic ' + base64.b64encode(username + ":" + password)},
                          allow_truncated=True)
-        res = urlfetch.fetch('http://lehrblogger.com/nyu/classes/spring09/a2z/midterm/testing.xml')                 
-        dom = parseString(res.content.partition('<!--')[0])
-        return dom.getElementsByTagName('post')
+        #res = urlfetch.fetch('http://lehrblogger.com/nyu/classes/spring09/a2z/midterm/testing.xml')
+        if res.content.find('Sorry, Unable to process request at this time -- error 999.') != -1:
+          print res.content
+          print '<br><br><br><br><br>YAHOO API FAIL<br><br><br><br><br>'
+        else:
+          dom = parseString(res.content.partition('<!--')[0])
+          return dom.getElementsByTagName('post')
   
     def get(self, testarg):
         bookmarks = self.searchHTTP('memento85', 'ont9oth1ag6foc') #get these later from datastore, they are being irgnored right now
+        urls = [bookmark.getAttribute('href').replace( '&', '&amp;') for bookmark in bookmarks]
+       
         self.response.headers['Content-Type'] = 'text/xml'
-
-        template_vars = {'bookmarks' : bookmarks}
+        
+        template_vars = {'urls' : urls}
         path = os.path.join(os.path.dirname(__file__), 'annotations.xml')
-
+        
         self.response.out.write(template.render(path, template_vars))
-
-
-
 
 
 application = webapp.WSGIApplication(
