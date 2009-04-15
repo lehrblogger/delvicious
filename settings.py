fe54c44e0ee34cba1865c13b74031d85aad73747
Steven <lehrburger@gmail.com>
2009-04-14 23:26:05 -0400

    just removing the delvi-icio-us folder, which i think is superfluous

--- settings.py
+++ settings.py
@@ -93,4 +93,14 @@ IGNORE_APP_SETTINGS = IGNORE_APP_URLSAUTO = (
     # 'yetanotherapp',
 )
 
+import os
+ROOT_PATH = os.path.dirname(__file__)
+TEMPLATE_DIRS = (
+    # Put strings here, like "/home/html/django_templates" or
+    # "C:/www/django/templates".  Always use forward slashes, even on Windows.
+    # Don't forget to use absolute paths, not relative paths.
+    ROOT_PATH + '/templates',
+)
+
+
 from ragendja.settings_post import *
